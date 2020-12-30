ecb8c5ffe41d6977e07bed41227312428a196dc9
sett17 <raik.rohde@gmail.com>
2020-12-31 00:23:45 +0100

    .

---
+++ drinkingGame/main.js
@@ -0,0 +1,12 @@
+document.querySelector('#root').innerHTML = loadFile('./compos/startmenu.compo')
+
+function loadFile(filePath) {
+    var result = null
+    var xmlhttp = new XMLHttpRequest()
+    xmlhttp.open("GET", filePath, false)
+    xmlhttp.send()
+    if (xmlhttp.status == 200) {
+        result = xmlhttp.responseText
+    }
+    return result
+}
---
+++ drinkingGame/service-worker.js
@@ -0,0 +1,50 @@
+cacheName = "testCache-v1.0"
+const toCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/assets/*',
+    '/compos/*',
+]
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(caches.open(cacheName).then(function (cache) {
+        return cache.match(event.request).then(function (response) {
+            console.log("[R] cache request: " + event.request.url)
+            var fetchPromise = fetch(event.request).then(function (networkResponse) {
+                console.log("[D] fetch completed: " + event.request.url, networkResponse)
+                if (networkResponse) {
+                    console.debug("[U] updated cached page: " + event.request.url, networkResponse)
+                    cache.put(event.request, networkResponse.clone())
+                }
+                return networkResponse
+            }, function (event) {
+                console.log("[E] Error in fetch()", event)
+                event.waitUntil(
+                    caches.open(cacheName).then(function (cache) {
+                        return cache.addAll(toCache)
+                    })
+                )
+            })
+            return response || fetchPromise
+        })
+    }))
+})
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== cacheName) {
+                    return caches.delete(key)
+                }
+            }))
+        })
+    )
+})
+
+self.addEventListener('install', function (event) {
+    // The promise that skipWaiting() returns can be safely ignored.
+    self.skipWaiting()
+    console.log("Latest version installed!")
+})
