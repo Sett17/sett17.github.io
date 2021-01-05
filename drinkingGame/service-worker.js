b83615bb46f384dff3d2f4157a469129bc1b8e79
sett17 <raik.rohde@gmail.com>
2021-01-05 03:41:19 +0100

    .

--- drinkingGame/service-worker.js
+++ drinkingGame/service-worker.js
@@ -67,7 +67,7 @@ self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(cacheName).then((cache) => {
             console.log('[Service Worker] Caching all: app shell and content')
-            return cache.addAll(contentToCache)
+            return cache.addAll(toCache)
         })
     )
 })
